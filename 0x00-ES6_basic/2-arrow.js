7759a69f5d3b24013be6941d8bfc86c77b245a9f
Rophpad <rophenp@gmail.com>
2024-03-04 17:08:55 +0100

    typo error corrected

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,4 +1,4 @@
-expot default function getNeighborhoodsList() {
+export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
