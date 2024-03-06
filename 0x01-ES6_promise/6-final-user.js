829df7970d1341dfb42ebef97d0544872e848a99
Rophpad <rophenp@gmail.com>
2024-03-06 16:05:27 +0100

    code updated

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,25 +1,25 @@
-import signUpUser from './4-user-promise'
-import uploadPhoto from './5-photo-reject'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-
-  const resPromise = Promise.allSettled(promises);
-  return resPromise.then((results) => {
-    const array = [];
-    result.forEach((result) => {
-      if (result.status === 'fulfilled') {
-        array.push({
-          status: result.status,
-	  value: result.value,
-	});
-      } else {
-        array.push({
-          status: index.status,
-	  value: `Error: ${index.reason.message}`,
-	});
-      }
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName)])
+    .then((results) => {
+      const array = [];
+      results.forEach((result) => {
+        if (result.status === 'fulfilled') {
+          array.push({
+            status: result.status,
+            value: result.value,
+          });
+        } else {
+          array.push({
+            status: result.status,
+            value: `Error: ${result.reason.message}`,
+          });
+        }
+      });
       return array;
     });
-  });
 }
